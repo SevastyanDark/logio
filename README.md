4611abb3e08d7d4a24ccaf528cae876bdaa94731
SevastyanDark <pigarev.sevastyan@gmail.com>
2018-03-06 11:39:18 +0700

    Add log levels

--- README.md
+++ README.md
@@ -2,19 +2,19 @@
 Simple and lightweight logger for Java/Kotlin/Scala ...
 ## Getting started
 To link logio to your project, use:
-```groovy
+```gradle
 dependencies
 {
-  compile 'com.github.sevastyandark:logio:0.1.0'
+    compile 'com.github.sevastyandark:logio:0.1.0'
 }
 ```
 or, using Maven:
 ```xml
-<dependency>
-  <groupId>com.github.sevastyandark</groupId>
-  <artifactId>logio</artifactId>
-  <version>0.1.0</version>
-</dependency>
+    <dependency>
+      <groupId>com.github.sevastyandark</groupId>
+      <artifactId>logio</artifactId>
+      <version>0.1.0</version>
+    </dependency>
 ```
 ## Examples
 
@@ -23,8 +23,10 @@ import com.github.sevastyandark.logio.*
 
 fun main(args: Array<String>)
 {
-    var log = LoggerFactory.getLog("main")
-    log.log(true)
-    log.log("Hello, world!!!!!")
+    var log = LoggerFactory.getLog("main") // Name is required
+
+    log.info("Hello, world!")
+    log.warn("Something look like to be wrong")
+    log.error("Something wrong")
 }
 ```
--- src/main/kotlin/com/github/sevastyandark/logio/Logger.kt
+++ src/main/kotlin/com/github/sevastyandark/logio/Logger.kt
@@ -1,11 +1,28 @@
+/*
+ * Copyright 2018      Sevastyan Pigarev
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ *
+ */
+
 package com.github.sevastyandark.logio
 
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
-class Logger(val name: String, val dateFormat: String = "yyyy-MM-dd HH:mm:ss.SSS", val printFormat: String = "[%date%] [%name%]: %text%")
+class Logger(val name: String, val dateFormat: String, val printFormat: String)
 {
-    fun log(x: Any)
+    fun info(x: Any)
     {
         val nowTime = LocalDateTime.now()
         val formatted = nowTime.format(DateTimeFormatter.ofPattern(dateFormat))
@@ -13,6 +30,51 @@ class Logger(val name: String, val dateFormat: String = "yyyy-MM-dd HH:mm:ss.SSS
         tmpFormat = tmpFormat.replace("%date%", formatted)
         tmpFormat = tmpFormat.replace("%name%", name)
         tmpFormat = tmpFormat.replace("%text%", x.toString())
+        tmpFormat = tmpFormat.replace("%log_level%", "INFO")
+        println(tmpFormat)
+    }
+    fun warn(x: Any)
+    {
+        val nowTime = LocalDateTime.now()
+        val formatted = nowTime.format(DateTimeFormatter.ofPattern(dateFormat))
+        var tmpFormat = printFormat
+        tmpFormat = tmpFormat.replace("%date%", formatted)
+        tmpFormat = tmpFormat.replace("%name%", name)
+        tmpFormat = tmpFormat.replace("%text%", x.toString())
+        tmpFormat = tmpFormat.replace("%log_level%", "WARN")
+        println(tmpFormat)
+    }
+    fun error(x: Any)
+    {
+        val nowTime = LocalDateTime.now()
+        val formatted = nowTime.format(DateTimeFormatter.ofPattern(dateFormat))
+        var tmpFormat = printFormat
+        tmpFormat = tmpFormat.replace("%date%", formatted)
+        tmpFormat = tmpFormat.replace("%name%", name)
+        tmpFormat = tmpFormat.replace("%text%", x.toString())
+        tmpFormat = tmpFormat.replace("%log_level%", "ERROR")
+        println(tmpFormat)
+    }
+    fun critical(x: Any)
+    {
+        val nowTime = LocalDateTime.now()
+        val formatted = nowTime.format(DateTimeFormatter.ofPattern(dateFormat))
+        var tmpFormat = printFormat
+        tmpFormat = tmpFormat.replace("%date%", formatted)
+        tmpFormat = tmpFormat.replace("%name%", name)
+        tmpFormat = tmpFormat.replace("%text%", x.toString())
+        tmpFormat = tmpFormat.replace("%log_level%", "CRITICAL")
+        println(tmpFormat)
+    }
+    fun debug(x: Any)
+    {
+        val nowTime = LocalDateTime.now()
+        val formatted = nowTime.format(DateTimeFormatter.ofPattern(dateFormat))
+        var tmpFormat = printFormat
+        tmpFormat = tmpFormat.replace("%date%", formatted)
+        tmpFormat = tmpFormat.replace("%name%", name)
+        tmpFormat = tmpFormat.replace("%text%", x.toString())
+        tmpFormat = tmpFormat.replace("%log_level%", "DEBUG")
         println(tmpFormat)
     }
 }
